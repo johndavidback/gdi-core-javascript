f8cf81e89f2029296dc32aa0db8874a243cdb560
John Back <johndavidback@gmail.com>
2013-03-11 20:42:03 -0400

    Updating classes and moving things forward

--- class1/exercise2/javascript.js
+++ class1/exercise2/javascript.js
@@ -1,11 +1,20 @@
-var age = 28;
-var oldAge = 60;
-var perDay = 2;
 
-var days = (oldAge - age) * 365;
-var total = perDay * days;
-if(total > 40000) {
-    alert("You will need " + total + " to last you until the ripe old age of " + oldAge + ". Wow! That's a lot!");
-} else {
-    alert("You will need " + total + " to last you until the ripe old age of " + oldAge + ". You seem pretty reasonable");
+function calculate() {
+
+    alert('Starting calculation...');
+    var age = 28;
+    var oldAge = 60;
+    var perDay = 2;
+
+    var days = (oldAge - age) * 365;
+    var total = perDay * days;
+    if(total > 40000) {
+        alert("You will need " + total + " to last you until the \
+            ripe old age of " + oldAge + ". Wow! That's a lot!");
+
+    } else {
+        alert("You will need " + total + " to last you until the \
+            ripe old age of " + oldAge + ". You seem pretty \
+            reasonable");
+    }
 }
